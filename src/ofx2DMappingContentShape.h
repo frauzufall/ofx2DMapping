2672ea366676083a1cd9bfd5ed30069383ac7c02
random <random@frauzufall.de>
2016-03-18 09:16:11 +0100

    spaces -> tabs (probably)

--- src/ofx2DMappingContentShape.h
+++ src/ofx2DMappingContentShape.h
@@ -4,155 +4,161 @@
 
 class ofx2DMappingContentShape : public ofx2DMappingShape {
 
-    public:
+	public:
 
-    ofPoint     src[4];
-    float       src_width;
-    float       src_height;
-    ofMatrix4x4 matrix_src_dst;
+	ofPoint     src[4];
+	float       src_width;
+	float       src_height;
+	ofMatrix4x4 matrix_src_dst;
 
-    ofTexture* texture;
+	ofTexture* texture;
 
-    ofx2DMappingContentShape() {
+	ofx2DMappingContentShape() {
 
-        this->nature = "CONTENT_SHAPE";
+		this->nature = "CONTENT_SHAPE";
 
-        this->src[0].x = 0;
-        this->src[0].y = 0;
-        this->src[1].x = 1;
-        this->src[1].y = this->src[0].y;
-        this->src[2].x = this->src[1].x;
-        this->src[2].y = 1;
-        this->src[3].x = this->src[0].x;
-        this->src[3].y = this->src[2].y;
+		this->src[0].x = 0;
+		this->src[0].y = 0;
+		this->src[1].x = 1;
+		this->src[1].y = this->src[0].y;
+		this->src[2].x = this->src[1].x;
+		this->src[2].y = 1;
+		this->src[3].x = this->src[0].x;
+		this->src[3].y = this->src[2].y;
 
-        this->matrix_src_dst.makeIdentityMatrix();
-    }
+		this->matrix_src_dst.makeIdentityMatrix();
 
-    ofx2DMappingContentShape(const ofx2DMappingContentShape& obj) : ofx2DMappingShape(obj) {
-        for(int i = 0; i < 4; i++) {
-            this->src[i] = obj.src[i];
-        }
-        this->src_width = obj.src_width;
-        this->src_height = obj.src_height;
-        this->matrix_src_dst = obj.matrix_src_dst;
-        this->texture = obj.texture;
-    }
+		this->texture = 0;
+	}
 
-    ofPtr<ofx2DMappingObject> clone() const {
-        return ofPtr<ofx2DMappingObject>(new ofx2DMappingContentShape(*this));
-    }
+	ofx2DMappingContentShape(const ofx2DMappingContentShape& obj) : ofx2DMappingShape(obj) {
+		for(int i = 0; i < 4; i++) {
+			this->src[i] = obj.src[i];
+		}
+		this->src_width = obj.src_width;
+		this->src_height = obj.src_height;
+		this->matrix_src_dst = obj.matrix_src_dst;
+		this->texture = obj.texture;
+	}
 
-    void loadXml(ofxXmlSettings_ptr xml) {
-        ofx2DMappingShape::loadXml(xml);
-        xml->pushTag("src", 0);
-            setRectangle(xml, src);
-        xml->popTag();
-    }
+	ofPtr<ofx2DMappingObject> clone() const {
+		return ofPtr<ofx2DMappingObject>(new ofx2DMappingContentShape(*this));
+	}
 
-    void saveXml(ofxXmlSettings_ptr xml) {
-        ofx2DMappingShape::saveXml(xml);
-        xml->addTag("src");
-        xml->pushTag("src", 0);
-            xml->addTag("lefttop");
-            xml->pushTag("lefttop", 0);
-                xml->addValue("x", src[0].x);
-                xml->addValue("y", src[0].y);
-            xml->popTag();
-            xml->addTag("righttop");
-            xml->pushTag("righttop", 0);
-                xml->addValue("x", src[1].x);
-                xml->addValue("y", src[1].y);
-            xml->popTag();
-            xml->addTag("rightbottom");
-            xml->pushTag("rightbottom", 0);
-                xml->addValue("x", src[2].x);
-                xml->addValue("y", src[2].y);
-            xml->popTag();
-            xml->addTag("leftbottom");
-            xml->pushTag("leftbottom", 0);
-                xml->addValue("x", src[3].x);
-                xml->addValue("y", src[3].y);
-            xml->popTag();
-        xml->popTag();
-    }
+	void loadXml(ofxXmlSettings_ptr xml) {
+		ofx2DMappingShape::loadXml(xml);
+		xml->pushTag("src", 0);
+			setRectangle(xml, src);
+		xml->popTag();
+	}
 
-    void update(float w, float h) {
-        ofx2DMappingShape::update(w, h);
-        this->findHomography(this->dst, this->src, (GLfloat*) this->matrix_src_dst.getPtr(), true, w, h);
-    }
+	void saveXml(ofxXmlSettings_ptr xml) {
+		ofx2DMappingShape::saveXml(xml);
+		xml->addTag("src");
+		xml->pushTag("src", 0);
+			xml->addTag("lefttop");
+			xml->pushTag("lefttop", 0);
+				xml->addValue("x", src[0].x);
+				xml->addValue("y", src[0].y);
+			xml->popTag();
+			xml->addTag("righttop");
+			xml->pushTag("righttop", 0);
+				xml->addValue("x", src[1].x);
+				xml->addValue("y", src[1].y);
+			xml->popTag();
+			xml->addTag("rightbottom");
+			xml->pushTag("rightbottom", 0);
+				xml->addValue("x", src[2].x);
+				xml->addValue("y", src[2].y);
+			xml->popTag();
+			xml->addTag("leftbottom");
+			xml->pushTag("leftbottom", 0);
+				xml->addValue("x", src[3].x);
+				xml->addValue("y", src[3].y);
+			xml->popTag();
+		xml->popTag();
+	}
 
-    void setTexture(ofTexture* texture) {
-        this->texture = texture;
-        if(texture) {
-            src_width = texture->getWidth();
-            src_height = texture->getHeight();
-        }
-    }
+	void update(float w, float h) {
+		ofx2DMappingShape::update(w, h);
+		this->findHomography(this->dst, this->src, (GLfloat*) this->matrix_src_dst.getPtr(), true, w, h);
+	}
 
-    void draw(float w, float h) {
+	void setTexture(ofTexture* texture) {
+		this->texture = texture;
+		if(texture) {
+			src_width = texture->getWidth();
+			src_height = texture->getHeight();
+		}
+	}
 
-        ofPushMatrix();
+	void draw(float w, float h) {
 
-            glMultMatrixf(this->matrix_dst_norm.getPtr());
+		ofPushMatrix();
 
-            if(this->texture) {
+			glMultMatrixf(this->matrix_dst_norm.getPtr());
 
-                ofSetColor(255);
+			if(this->texture != 0) {
 
-                texture->bind();
+				if(this->texture->isAllocated()) {
 
-                if(this->polyline.size()!= 4) {
+					ofSetColor(255);
 
-                    glBegin(GL_TRIANGLES);
-                    for (int i = 0; i < this->triangle.nTriangles; i++){
+					texture->bind();
 
-                        float cx = (((this->triangle.triangles[i].a.x - this->dst[0].x) / (this->dst[1].x-this->dst[0].x) * (this->src[1].x-this->src[0].x))+ this->src[0].x)* src_width;
-                        float cy = (((this->triangle.triangles[i].a.y - this->dst[0].y) / (this->dst[2].y-this->dst[0].y) * (this->src[2].y-this->src[0].y))+ this->src[0].y) * src_height;
-                        float ox = (this->triangle.triangles[i].a.x - this->dst[0].x) / (float)(this->dst[1].x-this->dst[0].x) * w;
-                        float oy = (this->triangle.triangles[i].a.y - this->dst[0].y) / (float)(this->dst[2].y-this->dst[0].y) * h;
-                        glTexCoord2f(cx, cy);
-                        glVertex2f(ox, oy);
+					if(this->polyline.size()!= 4) {
 
-                        cx = (((this->triangle.triangles[i].b.x - this->dst[0].x) / (this->dst[1].x-this->dst[0].x) * (this->src[1].x-this->src[0].x))+ this->src[0].x)* src_width;
-                        cy = (((this->triangle.triangles[i].b.y - this->dst[0].y) / (this->dst[2].y-this->dst[0].y) * (this->src[2].y-this->src[0].y))+ this->src[0].y) * src_height;
-                        ox = (this->triangle.triangles[i].b.x - this->dst[0].x)/(float)(this->dst[1].x-this->dst[0].x)*w;
-                        oy = (this->triangle.triangles[i].b.y - this->dst[0].y)/(float)(this->dst[2].y-this->dst[0].y)*h;
-                        glTexCoord2f(cx, cy);
-                        glVertex2f(ox, oy);
+						glBegin(GL_TRIANGLES);
+						for (int i = 0; i < this->triangle.nTriangles; i++){
 
-                        cx = (((this->triangle.triangles[i].c.x - this->dst[0].x) / (this->dst[1].x-this->dst[0].x) * (this->src[1].x-this->src[0].x))+ this->src[0].x)* src_width;
-                        cy = (((this->triangle.triangles[i].c.y - this->dst[0].y) / (this->dst[2].y-this->dst[0].y) * (this->src[2].y-this->src[0].y))+ this->src[0].y) * src_height;
-                        ox = (this->triangle.triangles[i].c.x - this->dst[0].x)/(float)(this->dst[1].x-this->dst[0].x)*w;
-                        oy = (this->triangle.triangles[i].c.y - this->dst[0].y)/(float)(this->dst[2].y-this->dst[0].y)*h;
-                        glTexCoord2f(cx, cy);
-                        glVertex2f(ox, oy);
+							float cx = (((this->triangle.triangles[i].a.x - this->dst[0].x) / (this->dst[1].x-this->dst[0].x) * (this->src[1].x-this->src[0].x))+ this->src[0].x)* src_width;
+							float cy = (((this->triangle.triangles[i].a.y - this->dst[0].y) / (this->dst[2].y-this->dst[0].y) * (this->src[2].y-this->src[0].y))+ this->src[0].y) * src_height;
+							float ox = (this->triangle.triangles[i].a.x - this->dst[0].x) / (float)(this->dst[1].x-this->dst[0].x) * w;
+							float oy = (this->triangle.triangles[i].a.y - this->dst[0].y) / (float)(this->dst[2].y-this->dst[0].y) * h;
+							glTexCoord2f(cx, cy);
+							glVertex2f(ox, oy);
 
-                    }
-                    glEnd();
-                }
-                else {
+							cx = (((this->triangle.triangles[i].b.x - this->dst[0].x) / (this->dst[1].x-this->dst[0].x) * (this->src[1].x-this->src[0].x))+ this->src[0].x)* src_width;
+							cy = (((this->triangle.triangles[i].b.y - this->dst[0].y) / (this->dst[2].y-this->dst[0].y) * (this->src[2].y-this->src[0].y))+ this->src[0].y) * src_height;
+							ox = (this->triangle.triangles[i].b.x - this->dst[0].x)/(float)(this->dst[1].x-this->dst[0].x)*w;
+							oy = (this->triangle.triangles[i].b.y - this->dst[0].y)/(float)(this->dst[2].y-this->dst[0].y)*h;
+							glTexCoord2f(cx, cy);
+							glVertex2f(ox, oy);
 
-                    glBegin(GL_QUADS);
+							cx = (((this->triangle.triangles[i].c.x - this->dst[0].x) / (this->dst[1].x-this->dst[0].x) * (this->src[1].x-this->src[0].x))+ this->src[0].x)* src_width;
+							cy = (((this->triangle.triangles[i].c.y - this->dst[0].y) / (this->dst[2].y-this->dst[0].y) * (this->src[2].y-this->src[0].y))+ this->src[0].y) * src_height;
+							ox = (this->triangle.triangles[i].c.x - this->dst[0].x)/(float)(this->dst[1].x-this->dst[0].x)*w;
+							oy = (this->triangle.triangles[i].c.y - this->dst[0].y)/(float)(this->dst[2].y-this->dst[0].y)*h;
+							glTexCoord2f(cx, cy);
+							glVertex2f(ox, oy);
 
-                    glTexCoord2f(this->src[0].x*src_width, this->src[0].y*src_height);		glVertex3f(0, 0, 0);
-                    glTexCoord2f(this->src[1].x*src_width, this->src[1].y*src_height);		glVertex3f(w, 0, 0);
-                    glTexCoord2f(this->src[2].x*src_width, this->src[2].y*src_height);		glVertex3f(w, h, 0);
-                    glTexCoord2f(this->src[3].x*src_width, this->src[3].y*src_height);		glVertex3f(0, h, 0);
+						}
+						glEnd();
+					}
+					else {
 
-                   glEnd();
-                }
+						glBegin(GL_QUADS);
 
-                texture->unbind();
+						glTexCoord2f(this->src[0].x*src_width, this->src[0].y*src_height);		glVertex3f(0, 0, 0);
+						glTexCoord2f(this->src[1].x*src_width, this->src[1].y*src_height);		glVertex3f(w, 0, 0);
+						glTexCoord2f(this->src[2].x*src_width, this->src[2].y*src_height);		glVertex3f(w, h, 0);
+						glTexCoord2f(this->src[3].x*src_width, this->src[3].y*src_height);		glVertex3f(0, h, 0);
 
-            }
+					   glEnd();
+					}
 
-        ofPopMatrix();
+					texture->unbind();
 
-    }
+				}
 
-    ofTexture* getTexture() {
-        return texture;
-    }
+			}
+
+		ofPopMatrix();
+
+	}
+
+	ofTexture* getTexture() {
+		return texture;
+	}
 
 };
--- src/ofx2DMappingObject.h
+++ src/ofx2DMappingObject.h
@@ -13,226 +13,247 @@ typedef ofPtr<ofxXmlSettings> ofxXmlSettings_ptr;
 class ofx2DMappingObject {
 public:
 
-    string id;
-    string  nature;
+	string id;
+	string  nature;
 
-    string name;
-    ofColor color;
+	string name;
+	ofColor color;
 
-    ofx2DMappingObject() {
-        stringstream id_str;
-        id_str << IDGenerator::getInstance().next();
-        id = id_str.str();
-        newpos = true;
-        newitem = true;
+	ofx2DMappingObject() {
+		stringstream id_str;
+		id_str << IDGenerator::getInstance().next();
+		id = id_str.str();
+		newpos = true;
+		newitem = true;
 
-        nature = "OBJECT";
+		nature = "OBJECT";
 
-        editable.set(nature, false);
-    }
+		editable.set(nature, false);
+	}
 
-    ofx2DMappingObject(const ofx2DMappingObject& obj) {
-        stringstream id_str;
-        id_str << IDGenerator::getInstance().next();
-        id = id_str.str();
-        newpos = true;
-        newitem = true;
-        this->color = obj.color;
-        this->name = obj.name;
-        this->nature = obj.nature;
-        this->newitem = true;
-        this->editable.set(obj.name, true);
-    }
+	ofx2DMappingObject(const ofx2DMappingObject& obj) {
+		stringstream id_str;
+		id_str << IDGenerator::getInstance().next();
+		id = id_str.str();
+		newpos = true;
+		newitem = true;
+		this->color = obj.color;
+		this->name = obj.name;
+		this->nature = obj.nature;
+		this->newitem = true;
+		this->editable.set(obj.name, true);
+	}
 
-    virtual ofPtr<ofx2DMappingObject> clone() const = 0;
+	virtual ofPtr<ofx2DMappingObject> clone() const = 0;
 
-    void setColor(ofColor color) {
-        this->color = color;
-    }
+	void setColor(ofColor color) {
+		this->color = color;
+	}
 
-    virtual void loadXml(ofxXmlSettings_ptr xml) {
-        color.r = xml->getAttribute("color", "r", 255, 0);
-        color.g = xml->getAttribute("color", "g", 255, 0);
-        color.b = xml->getAttribute("color", "b", 255, 0);
+	virtual void loadXml(ofxXmlSettings_ptr xml) {
+		color.r = xml->getAttribute("color", "r", 255, 0);
+		color.g = xml->getAttribute("color", "g", 255, 0);
+		color.b = xml->getAttribute("color", "b", 255, 0);
 
-    }
+	}
 
-    virtual void saveXml(ofxXmlSettings_ptr xml) {
-        xml->addTag("color");
-        xml->addAttribute("color", "r", color.r, 0);
-        xml->addAttribute("color", "g", color.g, 0);
-        xml->addAttribute("color", "b", color.b, 0);
-    }
+	virtual void saveXml(ofxXmlSettings_ptr xml) {
+		xml->addTag("color");
+		xml->addAttribute("color", "r", color.r, 0);
+		xml->addAttribute("color", "g", color.g, 0);
+		xml->addAttribute("color", "b", color.b, 0);
+	}
 
-    virtual void draw(float w, float h) = 0;
-    virtual void drawArea(float w, float h) = 0;
+	virtual void draw(float w, float h) = 0;
+	virtual void drawArea(float w, float h) = 0;
 
-    virtual void update(float w, float h) = 0;
+	virtual void update(float w, float h) = 0;
 
-    static void gaussian_elimination(float *input, int n)
-    {
-        // ported to c from pseudocode in
-        // http://en.wikipedia.org/wiki/Gaussian_elimination
+	static void gaussian_elimination(float *input, int n)
+	{
+		// ported to c from pseudocode in
+		// http://en.wikipedia.org/wiki/Gaussian_elimination
 
-        float * A = input;
-        int i = 0;
-        int j = 0;
-        int m = n-1;
-        while (i < m && j < n)
-        {
-            // Find pivot in column j, starting in row i:
-            int maxi = i;
-            for(int k = i+1; k<m; k++)
-            {
-                if(fabs(A[k*n+j]) > fabs(A[maxi*n+j]))
-                {
-                    maxi = k;
-                }
-            }
-            if (A[maxi*n+j] != 0)
-            {
-                //swap rows i and maxi, but do not change the value of i
-                if(i!=maxi)
-                    for(int k=0; k<n; k++)
-                    {
-                        float aux = A[i*n+k];
-                        A[i*n+k]=A[maxi*n+k];
-                        A[maxi*n+k]=aux;
-                    }
-                //Now A[i,j] will contain the old value of A[maxi,j].
-                //divide each entry in row i by A[i,j]
-                float A_ij=A[i*n+j];
-                for(int k=0; k<n; k++)
-                {
-                    A[i*n+k]/=A_ij;
-                }
-                //Now A[i,j] will have the value 1.
-                for(int u = i+1; u< m; u++)
-                {
-                    //subtract A[u,j] * row i from row u
-                    float A_uj = A[u*n+j];
-                    for(int k=0; k<n; k++)
-                    {
-                        A[u*n+k]-=A_uj*A[i*n+k];
-                    }
-                    //Now A[u,j] will be 0, since A[u,j] - A[i,j] * A[u,j] = A[u,j] - 1 * A[u,j] = 0.
-                }
+		float * A = input;
+		int i = 0;
+		int j = 0;
+		int m = n-1;
+		while (i < m && j < n)
+		{
+			// Find pivot in column j, starting in row i:
+			int maxi = i;
+			for(int k = i+1; k<m; k++)
+			{
+				if(fabs(A[k*n+j]) > fabs(A[maxi*n+j]))
+				{
+					maxi = k;
+				}
+			}
+			if (A[maxi*n+j] != 0)
+			{
+				//swap rows i and maxi, but do not change the value of i
+				if(i!=maxi)
+					for(int k=0; k<n; k++)
+					{
+						float aux = A[i*n+k];
+						A[i*n+k]=A[maxi*n+k];
+						A[maxi*n+k]=aux;
+					}
+				//Now A[i,j] will contain the old value of A[maxi,j].
+				//divide each entry in row i by A[i,j]
+				float A_ij=A[i*n+j];
+				for(int k=0; k<n; k++)
+				{
+					A[i*n+k]/=A_ij;
+				}
+				//Now A[i,j] will have the value 1.
+				for(int u = i+1; u< m; u++)
+				{
+					//subtract A[u,j] * row i from row u
+					float A_uj = A[u*n+j];
+					for(int k=0; k<n; k++)
+					{
+						A[u*n+k]-=A_uj*A[i*n+k];
+					}
+					//Now A[u,j] will be 0, since A[u,j] - A[i,j] * A[u,j] = A[u,j] - 1 * A[u,j] = 0.
+				}
 
-                i++;
-            }
-            j++;
-        }
+				i++;
+			}
+			j++;
+		}
 
-        //back substitution
-        for(int i=m-2; i>=0; i--)
-        {
-            for(int j=i+1; j<n-1; j++)
-            {
-                A[i*n+m]-=A[i*n+j]*A[j*n+m];
-                //A[i*n+j]=0;
-            }
-        }
-    }
+		//back substitution
+		for(int i=m-2; i>=0; i--)
+		{
+			for(int j=i+1; j<n-1; j++)
+			{
+				A[i*n+m]-=A[i*n+j]*A[j*n+m];
+				//A[i*n+j]=0;
+			}
+		}
+	}
 
-    static void findHomography(ofPolyline& line, ofPoint dst_norm[4], float homography[16], bool points_normalized, float w, float h){
-        ofPoint src_norm[4];
-        src_norm[0] = line[0];
-        src_norm[1] = line[1];
-        src_norm[2] = line[2];
-        src_norm[3] = line[3];
-        findHomography(src_norm, dst_norm, homography, points_normalized, w, h);
+	static void findHomography(ofPolyline& line, ofPoint dst_norm[4], float homography[16], bool points_normalized, float w, float h){
+		ofPoint src_norm[4];
+		src_norm[0] = line[0];
+		src_norm[1] = line[1];
+		src_norm[2] = line[2];
+		src_norm[3] = line[3];
+		findHomography(src_norm, dst_norm, homography, points_normalized, w, h);
 
-    }
+	}
 
-    static void findHomography(ofPoint src_norm[4], ofPoint dst_norm[4], float homography[16], bool points_normalized, float w, float h)
-    {
+	static ofMatrix4x4 findHomography(ofRectangle r1, ofRectangle r2){
+		ofMatrix4x4 m;
+		ofPoint src[4], dst[4];
+		src[0] = r1.getTopLeft();
+		src[1] = r1.getTopRight();
+		src[2] = r1.getBottomRight();
+		src[3] = r1.getBottomLeft();
+		dst[0] = r2.getTopLeft();
+		dst[1] = r2.getTopRight();
+		dst[2] = r2.getBottomRight();
+		dst[3] = r2.getBottomLeft();
+		findHomography(src, dst, (GLfloat*) m.getPtr(), false, 0, 0);
+		return m;
+	}
 
-        ofPoint src[4];
-        ofPoint dst[4];
+	static ofMatrix4x4 findHomography(ofPoint src_norm[4], ofPoint dst_norm[4]){
+		ofMatrix4x4 m;
+		findHomography(src_norm, dst_norm, (GLfloat*) m.getPtr(), false, 0, 0);
+		return m;
+	}
 
-        if(points_normalized) {
-            for(int i = 0; i < 4; i++) {
-                src[i].x=src_norm[i].x*w;
-                src[i].y=src_norm[i].y*h;
-                dst[i].x=dst_norm[i].x*w;
-                dst[i].y=dst_norm[i].y*h;
-            }
-        }
-        else {
-            for(int i = 0; i < 4; i++) {
-                src[i].x=src_norm[i].x;
-                src[i].y=src_norm[i].y;
-                dst[i].x=dst_norm[i].x;
-                dst[i].y=dst_norm[i].y;
-            }
-        }
+	static void findHomography(ofPoint src_norm[4], ofPoint dst_norm[4], float homography[16], bool points_normalized, float w, float h)
+	{
 
-        // create the equation system to be solved
-        //
-        // from: Multiple View Geometry in Computer Vision 2ed
-        //       Hartley R. and Zisserman A.
-        //
-        // x' = xH
-        // where H is the homography: a 3 by 3 matrix
-        // that transformed to inhomogeneous coordinates for each point
-        // gives the following equations for each point:
-        //
-        // x' * (h31*x + h32*y + h33) = h11*x + h12*y + h13
-        // y' * (h31*x + h32*y + h33) = h21*x + h22*y + h23
-        //
-        // as the homography is scale independent we can let h33 be 1 (indeed any of the terms)
-        // so for 4 points we have 8 equations for 8 terms to solve: h11 - h32
-        // after ordering the terms it gives the following matrix
-        // that can be solved with gaussian elimination:
+		ofPoint src[4];
+		ofPoint dst[4];
 
-        float P[8][9]=
-        {
-            {-src[0].x, -src[0].y, -1,   0,   0,  0, src[0].x*dst[0].x, src[0].y*dst[0].x, -dst[0].x }, // h11
-            {  0,   0,  0, -src[0].x, -src[0].y, -1, src[0].x*dst[0].y, src[0].y*dst[0].y, -dst[0].y }, // h12
+		if(points_normalized) {
+			for(int i = 0; i < 4; i++) {
+				src[i].x=src_norm[i].x*w;
+				src[i].y=src_norm[i].y*h;
+				dst[i].x=dst_norm[i].x*w;
+				dst[i].y=dst_norm[i].y*h;
+			}
+		}
+		else {
+			for(int i = 0; i < 4; i++) {
+				src[i].x=src_norm[i].x;
+				src[i].y=src_norm[i].y;
+				dst[i].x=dst_norm[i].x;
+				dst[i].y=dst_norm[i].y;
+			}
+		}
 
-            {-src[1].x, -src[1].y, -1,   0,   0,  0, src[1].x*dst[1].x, src[1].y*dst[1].x, -dst[1].x }, // h13
-            {  0,   0,  0, -src[1].x, -src[1].y, -1, src[1].x*dst[1].y, src[1].y*dst[1].y, -dst[1].y }, // h21
+		// create the equation system to be solved
+		//
+		// from: Multiple View Geometry in Computer Vision 2ed
+		//       Hartley R. and Zisserman A.
+		//
+		// x' = xH
+		// where H is the homography: a 3 by 3 matrix
+		// that transformed to inhomogeneous coordinates for each point
+		// gives the following equations for each point:
+		//
+		// x' * (h31*x + h32*y + h33) = h11*x + h12*y + h13
+		// y' * (h31*x + h32*y + h33) = h21*x + h22*y + h23
+		//
+		// as the homography is scale independent we can let h33 be 1 (indeed any of the terms)
+		// so for 4 points we have 8 equations for 8 terms to solve: h11 - h32
+		// after ordering the terms it gives the following matrix
+		// that can be solved with gaussian elimination:
 
-            {-src[2].x, -src[2].y, -1,   0,   0,  0, src[2].x*dst[2].x, src[2].y*dst[2].x, -dst[2].x }, // h22
-            {  0,   0,  0, -src[2].x, -src[2].y, -1, src[2].x*dst[2].y, src[2].y*dst[2].y, -dst[2].y }, // h23
+		float P[8][9]=
+		{
+			{-src[0].x, -src[0].y, -1,   0,   0,  0, src[0].x*dst[0].x, src[0].y*dst[0].x, -dst[0].x }, // h11
+			{  0,   0,  0, -src[0].x, -src[0].y, -1, src[0].x*dst[0].y, src[0].y*dst[0].y, -dst[0].y }, // h12
 
-            {-src[3].x, -src[3].y, -1,   0,   0,  0, src[3].x*dst[3].x, src[3].y*dst[3].x, -dst[3].x }, // h31
-            {  0,   0,  0, -src[3].x, -src[3].y, -1, src[3].x*dst[3].y, src[3].y*dst[3].y, -dst[3].y }, // h32
-        };
+			{-src[1].x, -src[1].y, -1,   0,   0,  0, src[1].x*dst[1].x, src[1].y*dst[1].x, -dst[1].x }, // h13
+			{  0,   0,  0, -src[1].x, -src[1].y, -1, src[1].x*dst[1].y, src[1].y*dst[1].y, -dst[1].y }, // h21
 
-        gaussian_elimination(&P[0][0],9);
+			{-src[2].x, -src[2].y, -1,   0,   0,  0, src[2].x*dst[2].x, src[2].y*dst[2].x, -dst[2].x }, // h22
+			{  0,   0,  0, -src[2].x, -src[2].y, -1, src[2].x*dst[2].y, src[2].y*dst[2].y, -dst[2].y }, // h23
 
-        // gaussian elimination gives the results of the equation system
-        // in the last column of the original matrix.
-        // opengl needs the transposed 4x4 matrix:
-        float aux_H[]= { P[0][8],P[3][8],0,P[6][8], // h11  h21 0 h31
-                         P[1][8],P[4][8],0,P[7][8], // h12  h22 0 h32
-                         0      ,      0,0,0,       // 0    0   0 0
-                         P[2][8],P[5][8],0,1
-                       };      // h13  h23 0 h33
+			{-src[3].x, -src[3].y, -1,   0,   0,  0, src[3].x*dst[3].x, src[3].y*dst[3].x, -dst[3].x }, // h31
+			{  0,   0,  0, -src[3].x, -src[3].y, -1, src[3].x*dst[3].y, src[3].y*dst[3].y, -dst[3].y }, // h32
+		};
 
-        for(int i=0; i<16; i++) homography[i] = aux_H[i];
-    }
+		gaussian_elimination(&P[0][0],9);
 
-    bool        newpos;
-    bool        newitem;
-    ofParameter<bool> editable;
-    ofParameter<bool> pleaseCopyMe;
+		// gaussian elimination gives the results of the equation system
+		// in the last column of the original matrix.
+		// opengl needs the transposed 4x4 matrix:
+		float aux_H[]= { P[0][8],P[3][8],0,P[6][8], // h11  h21 0 h31
+						 P[1][8],P[4][8],0,P[7][8], // h12  h22 0 h32
+						 0      ,      0,0,0,       // 0    0   0 0
+						 P[2][8],P[5][8],0,1
+					   };      // h13  h23 0 h33
 
-    ~ofx2DMappingObject(){}
+		for(int i=0; i<16; i++) homography[i] = aux_H[i];
+	}
+
+	bool        newpos;
+	bool        newitem;
+	ofParameter<bool> editable;
+	ofParameter<bool> pleaseCopyMe;
+
+	~ofx2DMappingObject(){}
 
 protected:
 
-    ofPoint getPoint(ofxXmlSettings_ptr xml) {
+	ofPoint getPoint(ofxXmlSettings_ptr xml) {
 
-        ofPoint res;
+		ofPoint res;
 
-        res.x = xml->getValue("x", 0.);
-        res.y = xml->getValue("y", 0.);
+		res.x = xml->getValue("x", 0.);
+		res.y = xml->getValue("y", 0.);
 
-        return res;
+		return res;
 
-    }
+	}
 
 };
 
